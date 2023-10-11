466bb484a03b312f3ebad7c00bc47e4a780576d9
Pedro Gomes <pgestudos2022@gmail.com>
2023-10-11 16:44:20 -0300

    ADD metodo DELETE

--- src/main/java/com/pedrogomesdev/todolist/services/TarefaService.java
+++ src/main/java/com/pedrogomesdev/todolist/services/TarefaService.java
@@ -16,34 +16,42 @@ public class TarefaService {
 
 	@Autowired
 	private TarefaRepository repository;
-	
+
 	private final ModelMapper modelMapper = new ModelMapper();
-	
+
 	@Transactional(readOnly = true)
 	public TarefaDTO methodGet(Long id) {
 		Tarefa entity = repository.findById(id).orElseThrow();
 		return new TarefaDTO(entity);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Page<TarefaDTO> methodGetAll(Pageable page) {
 		Page<Tarefa> entity = repository.findAll(page);
 		return entity.map(x -> new TarefaDTO(x));
 	}
-	
+
 	@Transactional
 	public TarefaDTO insert(TarefaDTO dto) {
 		Tarefa entity = modelMapper.map(dto, Tarefa.class);
 		entity = repository.save(entity);
 		return new TarefaDTO(entity);
 	}
-	
+
 	@Transactional
 	public TarefaDTO update(Long id, TarefaDTO dto) {
 		Tarefa entity = repository.getReferenceById(id);
 		entity = modelMapper.map(dto, Tarefa.class);
 		entity = repository.save(entity);
 		return new TarefaDTO(entity);
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+
+		}
+		repository.deleteById(id);
 	}
 
 }
